69a3035f9925b8f09a5ec0ea93fb5defd7451c2c
Kirisi15 <kkirisigan@gmail.com>
2024-12-22 22:33:59 +0530

    Ctreated vehicle Entity

---
+++ Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/FuelStationApplication.java
@@ -0,0 +1,13 @@
+package Fuel_Station.Fuel_Station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelStationApplication.class, args);
+	}
+
+}
---
+++ Fuel_Station/src/test/java/Fuel_Station/Fuel_Station/FuelStationApplicationTests.java
@@ -0,0 +1,13 @@
+package Fuel_Station.Fuel_Station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
