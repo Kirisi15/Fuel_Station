b597cbed925a1df04f986a6d34e2df8dadbc26d9
Kirisi15 <kkirisigan@gmail.com>
2024-12-22 22:06:21 +0530

    Created vehicle Entity

---
+++ Fuel-Station/src/main/java/Fuel/Station/Fuel/Station/FuelStationApplication.java
@@ -0,0 +1,13 @@
+package Fuel.Station.Fuel.Station;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelStationApplication.class, args);
+	}
+
+}
---
+++ Fuel-Station/src/test/java/Fuel/Station/Fuel/Station/FuelStationApplicationTests.java
@@ -0,0 +1,13 @@
+package Fuel.Station.Fuel.Station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuelStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
