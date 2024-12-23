d09b6dec491428a8f37f5b5157605573bb98d2b7
Thushajini <thushayoga1205@gmail.com>
2024-12-23 12:03:22 +0530

    change datatype

--- Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/Entity/VehicleEntity.java
+++ Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/Entity/VehicleEntity.java
@@ -8,7 +8,7 @@ public class VehicleEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String VehicleId;
+    private Long VehicleId;
     @Column(name="Vehicle_Number")
     private String VehicleNumber;
     @Column(name="Vehicle_Type")
@@ -19,18 +19,18 @@ public class VehicleEntity {
     public VehicleEntity() {
     }
 
-    public VehicleEntity(String vehicleId, String vehicleNumber, String vehicleType, String fuelType) {
+    public VehicleEntity(Long vehicleId, String vehicleNumber, String vehicleType, String fuelType) {
         VehicleId = vehicleId;
         VehicleNumber = vehicleNumber;
         VehicleType = vehicleType;
         FuelType = fuelType;
     }
 
-    public String getVehicleId() {
+    public Long getVehicleId() {
         return VehicleId;
     }
 
-    public void setVehicleId(String vehicleId) {
+    public void setVehicleId(Long vehicleId) {
         VehicleId = vehicleId;
     }
 
