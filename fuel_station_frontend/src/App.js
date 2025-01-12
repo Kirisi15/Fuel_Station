c1e4aa95bc528cf3ac7f42437c27b77fe16334ea
jashvanth370 <jashva25@gmail.com>
2025-01-12 22:54:19 +0530

    Add route for CustomerDashboard,CustomerRegistration in App.js

--- fuel_station_frontend/src/App.js
+++ fuel_station_frontend/src/App.js
@@ -7,6 +7,8 @@ import VehicleRegistration from "./vehicle/VehicleRegistration";
 import Navbar from "./components/Navbar";
 import OwnerRegistration from './owner/OwnerRegistration';
 import FuelStationRegistration from "./fuelStation/FuelStationRegistration";
+import CustomerRegistration from "./customer/CustomerRegistration";
+import CustomerDashboard from "./customer/CustomerDashboard";
 
 function App() {
   return (
@@ -16,8 +18,9 @@ function App() {
           <Route path="/" element={<h1>Welcome to the App</h1>} />
           <Route path="/ownerReg" element={<OwnerRegistration />} />
           <Route path="/stationReg" element={<FuelStationRegistration />} />
-          
+          <Route path="/cusReg" element={<CustomerRegistration/>}/>
           <Route path="/vehicleReg" element={<VehicleRegistration />} />
+          <Route path="/customerDash" element={<CustomerDashboard />} />
           
 
         </Routes>
