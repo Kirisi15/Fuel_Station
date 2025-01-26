22d64620e443ede46d4bed4b9dbb1b120b27cd1a
jashvanth370 <jashva25@gmail.com>
2025-01-26 14:59:31 +0530

    every week refresh

--- Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/Entity/TimePeriod.java
+++ Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/Entity/TimePeriod.java
@@ -1,9 +1,6 @@
 package Fuel_Station.Fuel_Station.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedBy;
@@ -22,6 +19,7 @@ import java.time.LocalDateTime;
 @Table(name = "time_period")
 public class TimePeriod {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "start")
