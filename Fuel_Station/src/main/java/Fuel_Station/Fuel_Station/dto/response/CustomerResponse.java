fa73bf7edc3c19fa1db47fd05c54927259e9491c
jashvanth370 <jashva25@gmail.com>
2025-01-27 14:11:36 +0530

    backend changes

--- Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/dto/response/CustomerResponse.java
+++ Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/dto/response/CustomerResponse.java
@@ -1,4 +1,15 @@
 package Fuel_Station.Fuel_Station.dto.response;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class CustomerResponse {
+    private Long customerId;
+    private String customerNIC;
+    private String customerName;
+    private  String customerEmail;
+    private String customerUsername;
 }
