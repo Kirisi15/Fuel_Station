96bc5e892c2af9dad42696e5ccb4a15ec6ab7cc0
sweeskaran <rsweeskaran@gmail.com>
2025-01-31 16:03:49 +0530

    Cors change in fuel_limit_controller file

--- Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/Controller/FuelLimitController.java
+++ Fuel_Station/src/main/java/Fuel_Station/Fuel_Station/Controller/FuelLimitController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:3000,http://localhost:8081"})
 @RequestMapping("api/fuelLimit")
 public class FuelLimitController {
 
